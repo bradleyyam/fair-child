f72bc5e903ed39aa183d09034dec7541b6f82e42
Cove Geary <covertgeary@gmail.com>
2021-03-30 17:37:32 -0400

    Added selu network (3/4 models done). Methods for training, saving, and loading all models.

--- src/models.py
+++ src/models.py
@@ -1,5 +1,9 @@
 import lightgbm as lgb
+import pickle
 import sklearn.linear_model
+import tensorflow as tf
+
+#################### Logistic regression ####################
 
 def build_logreg():
     '''
@@ -10,11 +14,11 @@ def build_logreg():
         solver='lbfgs', penalty='l2', tol=1e-4, C=1, max_iter=100, class_weight='balanced')
     return m
 
-def load_logreg():
-    raise NotImplementedError("todo")
+def fit_logreg(m, xtrain, ytrain):
+    m.fit(xtrain, ytrain)
 
-def save_logreg():
-    raise NotImplementedError("todo")
+
+#################### Boosting with LightGBM ####################
 
 def build_gbdt():
     '''
@@ -30,8 +34,87 @@ def build_gbdt():
     # Also n.b. we should double-check that the `is_unbalance` paramater works as expected
     return m
     
-def save_logreg():
+def fit_gbdt(m, xtrain, ytrain, xval, yval, verbose=100):
+    m.fit(xtrain, ytrain, eval_set=(xval, yval), eval_metric='auc', verbose=verbose)
+
+
+#################### Saving and loading as pickle, for the two above model types (not NNs) ####################
+
+def load_pickle(filename):
+    m = None
+    with open(filename, 'rb') as file:
+        m = pickle.load(file)
+    return m
+
+def save_pickle(m, name):
+    with open(name, 'wb') as file:
+        pickle.dump(m, file)
+
+#################### Neural nets! ####################
+
+def build_NN_lrelu():
     raise NotImplementedError("todo")
 
-def load_logreg():
-    raise NotImplementedError("todo")
+def build_NN_selu(input_len):
+    '''
+    Second neural net architecture used by the authors. References:
+    General models in Keras https://keras.io/guides/sequential_model/
+    selu and lecun normalization (Keras API) https://keras.io/api/layers/activations/
+    selu example from the original selu authors https://github.com/bioinf-jku/SNNs/blob/master/TF_2_x/MNIST-MLP-SELU.py
+    AlphaDropout https://keras.io/api/layers/regularization_layers/alpha_dropout/
+    '''
+    N_NODES = input_len
+    ALPHA_DROPOUT = 0.15
+    ADAM_LR = 0.001
+
+    # Input
+    selu_in = tf.keras.layers.Input(shape=(N_NODES,))
+    # Hidden 1
+    x = tf.keras.layers.Dense(N_NODES, kernel_initializer='lecun_normal', activation='selu', name='selu1')(selu_in)
+    x = tf.keras.layers.AlphaDropout(ALPHA_DROPOUT)(x)
+    # Hidden 2
+    x = tf.keras.layers.Dense(N_NODES, kernel_initializer='lecun_normal', activation='selu', name='selu2')(x)
+    x = tf.keras.layers.AlphaDropout(ALPHA_DROPOUT)(x)
+    # Hidden 3
+    x = tf.keras.layers.Dense(N_NODES, kernel_initializer='lecun_normal', activation='selu', name='selu3')(x)
+    x = tf.keras.layers.AlphaDropout(ALPHA_DROPOUT)(x)
+    # Hidden 4
+    x = tf.keras.layers.Dense(N_NODES, kernel_initializer='lecun_normal', activation='selu', name='selu4')(x)
+    x = tf.keras.layers.AlphaDropout(ALPHA_DROPOUT)(x)
+    # Output
+    selu_out = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+
+    m = tf.keras.Model(inputs=selu_in, outputs=selu_out, name='NN_selu')
+    m.compile(
+        loss='binary_crossentropy',
+        optimizer=tf.keras.optimizers.Adam(learning_rate=ADAM_LR),
+        metrics=['Accuracy', 'AUC'],
+    )
+    # model is now ready for fitting!
+    return m
+
+def fit_NN_lrelu(m, xtrain, ytrain, xval, yval):
+    # Gotta figure out batch size and epochs. ?????
+    raise NotImplementedError('todo')
+
+def fit_NN_selu(m, xtrain, ytrain, xval, yval):
+    return fit_NN(m, xtrain, ytrain, xval, yval, batch_size=256, epochs=10)
+
+def fit_NN(m, xtrain, ytrain, xval, yval, batch_size, epochs):
+    '''
+    Reference: https://keras.io/guides/training_with_built_in_methods/
+    '''
+    history = m.fit(
+        xtrain,
+        ytrain,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(xval, yval),  # Monitor our metrics after each epoch, just 'cause
+    )
+    return history
+
+def save_NN(m, name):
+    m.save(name)
+
+def load_NN(filename):
+    return tf.keras.models.load_model(filename)
